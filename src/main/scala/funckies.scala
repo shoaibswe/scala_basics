898e19f04747e6af658aa3c0003eb63a135f601e
shoaibswe <shoaibswe@gmail.com>
2022-09-19 00:08:57 +0600

    functions

---
+++ src/main/scala/funckies.scala
@@ -0,0 +1,5 @@
+object funckies {
+def add_fun(a:Int =10, b:Int =20): Int ={
+  return a+b
+}
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -10,9 +10,19 @@ def main(args: Array[String]):Unit={
   var fl = new for_loop;
 //  fl.forloop()
 
-  var mat_exp = new match_expr;
-  mat_exp.age; //() not given coz void returns, so () can be ommted
+//  var mat_exp = new match_expr;
+//  mat_exp.age; //() not given coz void returns, so () can be ommted
 
-  hello_object.hi() // object does not need to instanciate , can direct call
+//  hello_object.hi() // object does not need to instanciate , can direct call
+
+var x:Int=  funckies.add_fun() //uses default param 10,20
+var y:Int=  funckies.add_fun(3,5) //uses custom param
+  println(f"$x+$y="+x+y)
+  println(x+ ""+ y +"=" +x+y) //does not sum, just shows value
+  println(x+ ""+ y +"=" +(x+y))
+  println(x+ "+"+ y +"=" +(x+y)) //correct to calculate sum
+  println(f"$x+$y="+x+y) //does not sum, just shows value
+  println(f"$x+$y="+(x+y)) //correct to calculate sum
+print("Happy Birthday!")
 }
 }
