c4b0c55f689878dd878498f4122ebebd75460b1c
shoaibswe <shoaibswe@gmail.com>
2022-09-19 00:47:38 +0600

    Higher Order Functions

--- src/main/scala/funckies.scala
+++ src/main/scala/funckies.scala
@@ -2,7 +2,15 @@ object funckies {
 def add_fun(a:Int =10, b:Int =20): Int ={
   return a+b
 }
-
+  //anonymous
   var AnonymousFun=( a:Int ,  b:Int)=> a+b; //this is anonymous func
 
+  //higher order func, take func as args and return func as argument
+def ho_fun_calcs (
+            a :Double , b :Double, fun:(Double,Double)=>Double//func input & return type
+        ):Double =fun(a,b);
+
+def ho_fun_calcs_3params(
+a: Double, b: Double, c: Double, fun: (Double, Double) => Double ): Double = fun(fun(a, b),c);
+};
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,21 +1,22 @@
 object main {
 def main(args: Array[String]):Unit={
 
+  /*
   var whilellop = new while_loop();
-//  whilellop.scala_while()
+  whilellop.scala_while()
 
   var doloop = new do_while();
-//  doloop.do_loop()
+  doloop.do_loop()
 
   var fl = new for_loop;
-//  fl.forloop()
+  fl.forloop()
 
-//  var mat_exp = new match_expr;
-//  mat_exp.age; //() not given coz void returns, so () can be ommted
+  var mat_exp = new match_expr;
+  mat_exp.age; //() not given coz void returns, so () can be ommted
 
-//  hello_object.hi() // object does not need to instanciate , can direct call
+  hello_object.hi() // object does not need to instanciate , can direct call
 
-  /*
+
 var x:Int=  funckies.add_fun() //uses default param 10,20
 var y:Int=  funckies.add_fun(3,5) //uses custom param
   println(f"$x+$y="+x+y)
@@ -24,8 +25,23 @@ var y:Int=  funckies.add_fun(3,5) //uses custom param
   println(x+ "+"+ y +"=" +(x+y)) //correct to calculate sum
   println(f"$x+$y="+x+y) //does not sum, just shows value
   println(f"$x+$y="+(x+y)) //correct to calculate sum
-  */
+
 
   print(funckies.AnonymousFun(1,2)) //calling an anonymous fun
+*/
+  //higher oder functions
+var ho_fun_add:Double=  funckies.ho_fun_calcs(10,20,(a,b)=>(a+b))
+var ho_fun_mul:Double=  funckies.ho_fun_calcs(10,20,(a,b)=>(a*b))
+var ho_fun_min:Double=  funckies.ho_fun_calcs(10,20,(a,b)=>(a min b))
+println(ho_fun_add)
+  println(ho_fun_mul)
+  println(ho_fun_min)
+
+  var ho_3param_min:Double=  funckies.ho_fun_calcs_3params(90,80,50,(a,b)=>(a min b))
+  println(ho_3param_min)
+
+  //wildcart with Higheroder Func, more flexible, less code  😀
+  var ho_3param_min_wildcart:Double=  funckies.ho_fun_calcs_3params(90,80,50,_ min _)
+  println(ho_3param_min_wildcart)
 }
 }
